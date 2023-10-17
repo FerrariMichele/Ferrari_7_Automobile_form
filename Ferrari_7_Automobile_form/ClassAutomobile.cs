c7735e374957019999fc4c949e056ae44f6d4252
Ferrari.20190 <Ferrari.20190@E2-9-PC07>
2023-10-17 12:33:36 +0200

    Aggiungere i file di progetto.

---
+++ Ferrari_7_Automobile_form/ClassAutomobile.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferrari_7_Automobile_form
+{
+    class ClassAutomobile
+    {
+        protected int _speed;
+        protected int _gear;
+        protected bool _onOff;
+
+        public ClassAutomobile()
+        {
+            _speed = 0;
+            _gear = 0;
+            _onOff = false;
+        }
+        public int speed
+        { 
+            get { return _speed; } 
+            set { _speed = value; } 
+        }
+        public int gear
+        {
+            get { return _gear; }
+            set { _gear = value; }
+        }
+        public bool onOff
+        {
+            get { return _onOff; }
+            set { _onOff = value; }
+        }
+        public int AccendiAuto()
+        {
+            if (!onOff)
+            {
+                onOff = true;
+                return 1;
+            }
+            return 0;
+        }
+        public int SpegniAuto()
+        {
+            if (onOff) 
+            {
+                onOff = false;
+                return 1;
+            }
+            return 0;
+        }
+        public int Accelera()
+        {
+            if (onOff && gear > 0)
+            {
+
+            }
+            return 0;
+        }
+    }
+}
---
+++ Ferrari_7_Automobile_form/Form1.cs
@@ -0,0 +1,10 @@
+namespace Ferrari_7_Automobile_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
